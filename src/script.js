cf1b20838a850712f58f39b4eb5a75c6b603a2fc
Alex <alexbachurin91@gmail.com>
2021-04-23 22:23:13 +0300

    tabs project

---
+++ src/script.js
@@ -0,0 +1,38 @@
+window.addEventListener('DOMContentLoaded', () => {
+    //get elems
+    const tabLinks = document.querySelectorAll('.about__link'),
+          tabLinkWrapper = document.querySelector('.about__links-wrapper'),
+          tabContent = document.querySelectorAll('.about__content-item'),
+          activeLinkClass = 'active-link',
+          activeContentClass = 'active-content';
+          
+    //show tab content
+    function showTabsContent(i) {
+        tabContent[i].classList.add(activeContentClass);
+        tabLinks[i].classList.add(activeLinkClass);
+    }
+
+    //hide tabs content
+    function hideTabsContent() {
+        tabContent.forEach(tab => {
+            tab.classList.remove(activeContentClass);
+        })
+        tabLinks.forEach(link => {
+            link.classList.remove(activeLinkClass);
+        })
+    }
+
+    //add event listener
+    tabLinkWrapper.addEventListener('click', (e) => {
+        const target = e.target;
+
+        if (target && target.classList.contains('about__link')) {
+            tabLinks.forEach((tab,i) => {
+                if (target === tab) {
+                    hideTabsContent();
+                    showTabsContent(i);
+                }
+            })
+        }
+    })
+})
